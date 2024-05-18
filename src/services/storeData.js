2e9ffceb697561ff23849cc6952aae571290890e
carissa-chandra <carissa.tjan03@gmail.com>
2024-05-18 10:21:13 +0700

    update collection name

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -8,13 +8,13 @@ const db = new Firestore({
 
 //store data prediction
 async function storeData(id, data) {
-    const predictCollection = db.collection('prediction');
+    const predictCollection = db.collection('predictions');
     return predictCollection.doc(id).set(data);
 }
 
 //retrieve history prediction data
 async function fetchAllData() {
-    const predictCollection = db.collection('prediction');
+    const predictCollection = db.collection('predictions');
     const snapshot = await predictCollection.get();
     if (snapshot.empty) {
         console.log('No matching documents.');
